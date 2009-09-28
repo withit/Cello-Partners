3bf612ba0c94a69aab75922a5b96ddb52622bd33
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-09-28 13:54:33 +1000

    add test for accessing the home page without authetncation

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  context "on get to index" do
+  context "on get to index when authenticated" do
     setup { get :index, {}, :user_id => Factory(:user).id.to_s }
     should_respond_with :success
+  end
+  
+  context "on get to index when not authenticated" do
+    setup { get :index, {}}
+    should_respond_with :redirect
+    should_redirect_to('login'){login_path}
   end
 end
