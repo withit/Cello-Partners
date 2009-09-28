f6977783ff36d4a35fd8093ac75dbcfd57c843b6
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-09-28 13:52:44 +1000

    replaced login logic with explictly passing in the session hash

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,18 +1,8 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  context "when logged in" do
-    setup{ login }
-    context "on get to index" do
-      setup { get :index }
-      should_respond_with :success
-    end
-  end
-  
-  def login
-    get :index
-    UserSession.controller = @controller
-    user = Factory(:user)
-    UserSession.new(:username => user.username, :password => user.new_password).save
+  context "on get to index" do
+    setup { get :index, {}, :user_id => Factory(:user).id.to_s }
+    should_respond_with :success
   end
 end
