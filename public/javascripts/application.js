bc584ef1283ec31692584a1a39e1f65ef86f3d88
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-09-28 12:55:59 +1000

    initial work on side menu

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+document.observe('dom:loaded', function(){
+  $$('.sidebar>ul>li').invoke('observe','click', function(e){
+    e.element().down('ul').toggle();
+  });
+});
