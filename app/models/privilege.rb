c3db3c114e9462fd91e61495913138d4e41fc8a0
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-09-28 15:12:52 +1000

    add user_id= method so that association biulding works

--- app/models/privilege.rb
+++ app/models/privilege.rb
@@ -3,4 +3,8 @@ class Privilege < ActiveRecord::Base
   set_primary_key :Access_ID
   belongs_to :user, :foreign_key => 'User_ID'
   belongs_to :role, :foreign_key => 'Group_ID'
+  
+  def user_id= user_id
+    self.User_ID= user_id
+  end
 end
