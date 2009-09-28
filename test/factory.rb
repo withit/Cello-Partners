8df0dfeeb5a0db319648197c53d6af341f62b752
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-09-28 13:42:21 +1000

    test for home page

---
+++ test/factory.rb
@@ -0,0 +1,4 @@
+Factory.define(:user) do |u|
+  u.username "test_user"
+  u.new_password "top$ecret"
+end
--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "when logged in" do
+    setup{ login }
+    context "on get to index" do
+      setup { get :index }
+      should_respond_with :success
+    end
+  end
+  
+  def login
+    get :index
+    UserSession.controller = @controller
+    user = Factory(:user)
+    UserSession.new(:username => user.username, :password => user.new_password).save
   end
 end
