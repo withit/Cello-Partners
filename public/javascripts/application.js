130aad89eb12e5d46d390f8758268296dd35685e
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-09-30 12:50:18 +1000

    make menu work using add/remove class on hover

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 document.observe('dom:loaded', function(){
-  $$('.sidebar>ul>li').invoke('observe','click', function(e){
-   $$('.sidebar>ul>li>ul').without(e.element().down('ul')).invoke('hide');
-   if(e.element().down('ul')){
-     e.element().down('ul').toggle();
-   }
-  });
   $$('#customer_select input[type=text]').invoke('observe','keyup', function(e){
     var value = e.element().value.toLowerCase();
     var option_to_select = e.element().next('select').select('option').find(function(e){
@@ -34,5 +28,19 @@ document.observe('dom:loaded', function(){
   $$('form#customer_select').invoke('observe','submit', function(e){
     e.stop();
     document.location = "/organisations/" + e.element().down('select').value + "/quotes/new" ;
+  });
+  $$('.sidebar').invoke('observe', 'mouseout',function(e){
+    e.findElement('li').removeClassName('active');
+    if(e.findElement('li').up('li'))
+    {
+      e.findElement('li').up('li').removeClassName('active');
+    }
+  });
+  $$('.sidebar').invoke('observe', 'mouseover',function(e){
+    e.findElement('li').addClassName('active');
+    if(e.findElement('li').up('li'))
+    {
+      e.findElement('li').up('li').addClassName('active');
+    }
   });
 });
