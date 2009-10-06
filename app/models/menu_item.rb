e4f36a000c8b741d44792194acbf40ae81580dde
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-10-06 15:22:32 +1100

    removed unused code from menu item

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,71 +1,4 @@
 class MenuItem  
-  #attr_reader :label, :permission
-  #attr_accessor :children
-  # def initialize grant, permission
-  #   @permission = permission
-  #   @label = grant.Label
-  #   @children = []
-  # end
-  
-  def sort
-    @permission.sort || -1
-  end
-  
-  def parent
-    @permission.parent
-  end
-  
-  def root?
-    permission.parent && permission.parent.zero?
-  end
-  
-  def <=> other
-    (sort <=> other.sort).nonzero? || label <=> other.label
-  end
-  
-  
-  def == other
-    label.to_s.downcase == other.label.to_s.downcase && permission == other.permission
-  end
-  
-  def add_child menu_item
-    @children << menu_item unless @children.include? menu_item
-  end
-  
-  # def children
-  #   @children.sort
-  # end
-  
-  def module 
-    permission.action.module
-  end
-  
-  def function
-    permission.action.function
-  end
-  
-  def url
-    return unless permission.action
-    case 
-    when self.module == 'shell_editmyself'
-      lambda{edit_profile_path}
-    when self.module == 'shell_user_mgr' && self.function == 'nrform'
-      lambda{new_user_path}
-    when self.module == 'shell_user_mgr' && self.function == 'sform'
-      lambda{search_users_path}
-    when self.module == 'quotes' && self.function == 'nrform'
-      lambda{new_quote_path}
-    when self.module == 'quotes' && self.function == 'sform'
-      lambda{search_quotes_path}
-    when self.module == 'shell_group_mgr' && self.function == 'nrform'
-      lambda{new_role_path}
-    when self.module == 'shell_group_mgr' && self.function == 'edit'
-      lambda{roles_path}
-    else 
-      nil
-    end
-  end
-  
   def self.find
     Permission.scoped(
       :joins => [:grants, :action], 
@@ -107,22 +40,4 @@ class MenuItem
   end
   
   attr_reader :children
-  # def find_child_menu_items
-  #   Permission.scoped(
-  #     :joins => [:grants, :action], 
-  #     :conditions => ["shell_permissions.User_Group_ID IN (?) and shell_permissions.is_menu = ? and shell_option_sets.parent != 0", role_ids.uniq, true],
-  #     :select => "shell_permissions.*, shell_option_sets.*, shell_options.*"
-  #   )
-  # end
-  # 
-  # def find_root_menu_items
-  #   Permission.scoped(
-  #     :joins => [:grants, :action], 
-  #     :conditions => ["shell_permissions.User_Group_ID IN (?) and shell_permissions.is_menu = ? and shell_option_sets.parent = 0", role_ids.uniq, true],
-  #     :select => "shell_permissions.*, shell_option_sets.*, shell_options.*"
-  #   )
-  # end
-  
-  
-  
 end
