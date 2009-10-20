568e504a53018db576d4753ecaf1bf4d5dc85ad9
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-10-20 14:52:08 +1100

    add sinatra wrapper around mssql database

---
+++ sinatra/sap.rb
@@ -0,0 +1,26 @@
+require 'rubygems'
+require 'sinatra'
+require 'ruby-odbc-0.9997/odbc'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  #:adapter => "jdbc",
+  #:driver => "com.microsoft.sqlserver.jdbc.SQLServerDriver",
+  #:url => "jdbc:sqlserver://fermat.wi.com.au:1433;databaseName=cello_sap;",
+  :username => "sa",
+  :password => "dg90p",
+  :adapter  => "odbc",
+  :dsn      => "cello_sap"
+)
+
+class Stock < ActiveRecord::Base
+  set_table_name "OITW"
+  
+  def self.find_by_item_code! code
+    find(:first, :conditions => {:ItemCode => code}) || raise(Sinatra::NotFound)
+  end
+end
+
+get '/stocks/:id.json' do
+  Stock.find_by_item_code!(params[:id]).to_json
+end
