e4e9896fc4e4612579e6a5881984c144ae142e91
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-10-28 09:44:20 +1100

    sort the pricing groups by group name and sort

--- app/controllers/pricing_groups_controller.rb
+++ app/controllers/pricing_groups_controller.rb
@@ -1,3 +1,8 @@
 class PricingGroupsController < ApplicationController
   include ValueLists
+  
+  def load_all_objects
+    @current_objects =  current_model.all(:joins => :pricing_group_name, :order => "pricing_group_names.name, sort")
+    instance_variable_set "@#{plural_instance_variable_name}", @current_objects
+  end
 end
