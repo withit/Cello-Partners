2d842334a6cbb081d4008a783b6ae5ae231f4cbd
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-11-13 14:56:46 +1100

    add symlinking of article documents

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,13 +4,15 @@ set :repository,  "git@github.com:withit/Cello-Partners.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, '192.168.8.10'
-role :app, "192.168.8.10"                          # This may be the same as your `Web` server
-role :db,  "192.168.8.10", :primary => true # This is where Rails migrations will run
+role :web, '202.63.65.20'
+role :app, "202.63.65.20"                          # This may be the same as your `Web` server
+role :db,  "202.63.65.20", :primary => true # This is where Rails migrations will run
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
+set :deploy_via, :copy
+set :copy_compression, :gzip
 
 set :deploy_to, "/Library/WebServer/#{application}"
 
@@ -22,7 +24,7 @@ set :group, 'admin'
 namespace :deploy do
   task :start, :roles => :app  do
     mongrel_ports.each do |port|
-      sudo "#{mongrel_command} start -p #{port} -e production \
+      sudo "#{mongrel_cmd} start -p #{port} -e production \
         --user #{user} --group #{group} -c #{current_path}"
     end
   end
@@ -36,6 +38,13 @@ namespace :deploy do
     start
   end
 end
+
+task :symlink_documents, :roles => :app do
+  run "rm -rf #{release_path}/documents"
+  run "ln -nfs #{shared_path}/documents #{release_path}"
+end
+
+after :update_code, :symlink_documents
 #   task :restart, :roles => :app, :except => { :no_release => true } do
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
