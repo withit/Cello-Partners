0fa7b6ac47da3f8eb33d29d7b53bc72f60e8943a
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-11-17 09:46:14 +1100

    fix symlinking

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,7 +44,7 @@ task :symlink_documents, :roles => :app do
   run "ln -nfs #{shared_path}/documents #{release_path}"
 end
 
-after :update_code, :symlink_documents
+after 'deploy:update_code', :symlink_documents
 #   task :restart, :roles => :app, :except => { :no_release => true } do
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
