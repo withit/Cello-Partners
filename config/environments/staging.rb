dc44d5819f01273168aafa9769fcf5f41bc53726
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-11-18 09:50:25 +1100

    configure smtp for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -14,5 +14,12 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = false
+# config.action_mailer.raise_delivery_errors = false
 
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+  :address => 'smtp.wi.com.au',
+  :port => 25,
+  :domain => 'wi.com.au'
+}
