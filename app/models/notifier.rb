9b870dfbde93486f86487ca2c4c31e5cf6b6b64f
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-11-19 11:22:54 +1100

    use the quotes orgnaisation in email subject

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,7 +2,8 @@ class Notifier < ActionMailer::Base
   def quote quote
     recipients quote.email.blank? ? (UserSession.find && UserSession.find.user).email : quote.email
     from       "partners@cello.com.au"
-    subject    "Cello quote for #{quote.grade_name} #{quote.calliper} um #{quote.gsm} gsm"
+    cc         "sales@cello.com.au" if quote.is_a?(Order)
+    subject    "#{quote.organisation_name} quote for #{quote.grade_name} #{quote.calliper} um #{quote.gsm} gsm"
     body       :quote => quote
     content_type "text/html"
   end
