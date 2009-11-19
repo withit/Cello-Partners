b312d8fb15c9e332ef50221070f85d61feaed94b
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-11-19 12:22:05 +1100

    set colours for the report emails

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,8 +2,8 @@ module OrdersHelper
   def color_for status
     case status
     when :available then '#96D56C'
-    when :not_available then '#AAAAAAA'
-    when :limited then "#EE1C25"
+    when :not_available then '#EE1C25'
+    when :limited then "#FCA61B"
     end
   end
 end
