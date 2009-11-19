9abe6ed70a3959b38ba4ddff30287761e5dd763b
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-11-19 12:37:25 +1100

    call a order an order in emails

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -4,7 +4,7 @@ class Notifier < ActionMailer::Base
     recipients quote.email.blank? ? (UserSession.find && UserSession.find.user).email : quote.email
     from       "partners@cello.com.au"
     cc         "sales@cello.com.au" if quote.is_a?(Order)
-    subject    "#{quote.organisation_name} quote for #{quote.grade_name} #{quote.calliper} um #{quote.gsm} gsm"
+    subject    "#{quote.organisation_name} #{quote.class.to_s.downcase} for #{quote.grade_name} #{quote.calliper} um #{quote.gsm} gsm"
     body       :quote => quote
     content_type "text/html"
   end
