2dcd3724ffe4638ee7e287de15b9bd47c06c11ac
Lachlan Sylvester <lachlan.sylvester@wi.com.au>
2009-11-20 12:33:26 +1100

    send 404 if file has not been upload - as per old site

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,6 +2,7 @@ class DocumentsController < ApplicationController
   def show
     @article = Article.find(params[:article_id])
     @document = @article.find_document(params[:id])
+    raise ActiveRecord::RecordNotFound unless File.exists?(@article.document_path(@document))
     send_file @article.document_path(@document), :filename => @document.name_with_extention
   end
   
