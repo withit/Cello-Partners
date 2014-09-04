5279351a518bcbac2c1350765a1ea495623015cc
Grant Sayer <grantsayer@gmail.com>
2014-09-04 14:00:39 +1000

    Update

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.12' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -15,11 +15,13 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
   config.gem 'haml'
   config.gem 'searchlogic'
-  config.gem 'mislav-will_paginate', :lib => 'will_paginate'
+#  config.gem 'mislav-will_paginate', :lib => 'will_paginate'
   config.gem "fastercsv"
-  config.gem 'binarylogic-settingslogic', :lib => 'settingslogic'
+#  config.gem 'binarylogic-settingslogic', :lib => 'settingslogic'
   config.gem 'javan-whenever', :lib => false, :source => 'http://gems.github.com'
   config.gem 'google_analytics'
+
+
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
